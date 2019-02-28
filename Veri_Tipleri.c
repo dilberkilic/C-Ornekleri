47a92d76d366b3d565893beb2659a694fc6bf388
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2019-03-01 01:49:08 +0300

    Veri türlerinin ne kadarlık yer kapladığını gösteren program.
    Dilber KILIC

--- Veri_Tipleri.c
+++ Veri_Tipleri.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int main()
 {
-//veri tiplerinin bilgisayarda ne kadar bitlik yer kapladigini gosterir.
+//veri tiplerinin bilgisayarda ne kadar bitlik yer kapladigini gosterir .
 	printf("long:%d\n", sizeof(long));
 	printf("short:%d\n",sizeof(short));
 	printf("int:%d\n", sizeof(int));
