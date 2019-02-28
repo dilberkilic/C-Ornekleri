a7aa9a6127b13aa5d92e4c4d32d1be8137502e36
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2019-02-28 15:30:30 +0300

    Ekrana merhaba dünya yazdıran program.
    Dilber KILIC

---
+++ Merhaba_Dunya.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // Ekrana yazdirma "printf" kullanicidan veri alma "scanf" gibi fonksiyonların bulundugu kutuphanedir.
+#include <stdlib.h> // System gibi fonksiyonlar yer alır.
+
+int main ()
+{ 
+	printf("merhaba dunya\n"); // \n (new line ) alt satira gecmek icin kullanilir.
+	system("pause);
+	return 0;
+}
