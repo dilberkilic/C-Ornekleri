a10ddd0cf3a703416708a9479695ec5c72187803
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2019-02-28 03:26:16 +0300

    For Dongusu
    Dilber KILIC

---
+++ For_Dongusu.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+// for(baslangic degeri ; kosul ; artis miktari) { dongude calisilmasi istenen kodlar buraya yazilir }
+	for (int i = 1; i < 11; i++) // 1 den 11 e kadar olan sayilari ekrana yazar.
+	{
+		printf("%d\n", i);
+	}
+	system("pause");
+	return 0;
+}
