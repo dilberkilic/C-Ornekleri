ac955de7c37d4bf642a8596823208ef823287134
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2019-02-28 15:36:34 +0300

    Veri tiplerinin ne kadarlık bitten oluştuğunu gösteren program.
    Dilber KILIC

---
+++ Veri_Tipleri.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+//veri tiplerinin bilgisayarda ne kadar bitlik yer kapladigini gosterir.
+	printf("long:%d\n", sizeof(long));
+	printf("short:%d\n",sizeof(short));
+	printf("int:%d\n", sizeof(int));
+	printf("float:%d\n", sizeof(float));
+	printf("double:%d\n", sizeof(double));
+	printf("char:%d\n", sizeof(char));
+	system("pause");
+	return 0;
+}
