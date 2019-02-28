b610607636acebe8eb446a41808dee1c9ef0c86d
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2019-03-01 01:28:14 +0300

    Girilen iki sayıya dört işlem uygulayan program.

---
+++ Dort_Islem.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	//2 sayi alalim , 4 islem sonuclarini ekrana yazdiralim.
+	int  sa1, sa2;
+	printf("iki sayi giriniz:");
+	scanf_s("%d %d", &sa1, &sa2);
+	printf("toplam:%d\n", sa1 + sa2);
+	printf("fark:%d\n", sa1 - sa2);
+	printf("carpim:%d\n", sa1*sa2);
+	printf("bolum:%f\n", ((float)sa1 / sa2)); // Eger  floata cevirmeseydik program hata verecekti bunun nedeni sayilarimizi int olarak  ama bolumu float olarak tanitmamizdir.
+	printf("mod:%d\n", sa1 % sa2);
+	system("pause");
+	return 0;
+}
