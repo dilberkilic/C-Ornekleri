1395e3a14c7c8c4174be427f248d532a39408612
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2019-03-01 01:57:46 +0300

    Kullanıcı tarafından girilen iki sayıya 4 işlem uygulayan program.
    Dilber KILIC

--- Dort_Islem.c
+++ Dort_Islem.c
@@ -9,7 +9,7 @@ int main() {
 	printf("fark:%d\n", sa1 - sa2);
 	printf("carpim:%d\n", sa1*sa2);
 	printf("bolum:%f\n", ((float)sa1 / sa2)); // Eger  floata cevirmeseydik program hata verecekti bunun nedeni sayilarimizi int olarak  ama bolumu float olarak tanitmamizdir.
-	printf("mod:%d\n", sa1 % sa2);
+	printf("mod:%d\n", sa1 % sa2); 
 	system("pause");
 	return 0;
 }
