d79aef3342fd89960d3447d86667848ad5d97d5c
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2019-03-01 04:03:38 +0300

    3 ün katları hariç 1 den 50 ye kadar yazan program.
    Dilber KILIC

---
+++ Break_Ve_Continue.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int sayi = 0;
+	while (true) // boyle yazarsanız dongu sonsuz olur.
+	{
+		
+		sayi++;
+
+		if (sayi % 3 == 0)
+			continue; // continue istenmeyen adimlarin atlanmasini saglar burada 3 e bolunen sayıları almayacak. 
+
+		if (sayi > 50)
+		{
+			break; // break donguden istenilen bri anda cikmamizi saglar burda sayi 50 den buyuk oldugunda program sonlanacak demek.
+		}
+	
+		printf("%d ", sayi);
+	}
+	system("pause");
+	return 0;
+}
